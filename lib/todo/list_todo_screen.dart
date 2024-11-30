fdd68fedd297f3646b20aef046f9015e1c734fb1
yunweneric <yunweneric@gmail.com>
2024-11-30 03:16:44 +0100

    added todos ui and card but unimplemeted

--- lib/todo/list_todo_screen.dart
+++ lib/todo/list_todo_screen.dart
@@ -1,4 +1,7 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:my_amplify_app/models/Todo.dart';
 
 class ListTodoScreen extends StatefulWidget {
   const ListTodoScreen({super.key});
@@ -8,8 +11,60 @@ class ListTodoScreen extends StatefulWidget {
 }
 
 class _ListTodoScreenState extends State<ListTodoScreen> {
+  List<Todo> _todos = [];
+  Future<void> _refreshTodos() async {
+    try {
+      final request = ModelQueries.list(Todo.classType);
+      final response = await Amplify.API.query(request: request).response;
+
+      final todos = response.data?.items;
+      if (response.hasErrors) {
+        safePrint('errors: ${response.errors}');
+        return;
+      }
+      setState(() {
+        safePrint(todos);
+        _todos = todos!.whereType<Todo>().toList();
+      });
+    } on ApiException catch (e) {
+      safePrint('Query failed: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshTodos();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: _todos.isEmpty
+          ? Center(child: Text("No todos"))
+          : ListView.separated(
+              itemCount: _todos.length,
+              separatorBuilder: (c, i) => SizedBox(height: 10),
+              itemBuilder: (c, i) {
+                return TodoCard(
+                  todo: _todos[i],
+                );
+              },
+            ),
+    );
+  }
+}
+
+class TodoCard extends StatelessWidget {
+  final Todo todo;
+  const TodoCard({super.key, required this.todo});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
